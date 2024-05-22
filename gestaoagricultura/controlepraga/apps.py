349d28bd4a300740f631d14b1e6cac34c98ac780
Marcos-de-Assis <marcosde123assis@gmail.com>
2024-05-21 22:46:24 -0300

    realizar mais uma tentativa de subir apps

---
+++ gestaoagricultura/controlepraga/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ControlepragaConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "controlepraga"
---
+++ gestaoagricultura/metacrescimento/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MetacrescimentoConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "metacrescimento"
